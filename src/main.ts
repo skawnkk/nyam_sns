f7949a7e3337b15b9d6f3e132a17ee08d0ba67a9
김남주 <skawn11114@gmail.com>
2024-03-22 09:05:15 +0900

    feat: dto whitelist 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true, //요청값을 자동으로 변환 @IsNumber => number로 인식,  @Type(() => Number)을 사용할 필요가 없다.
       },
+      whitelist: true, //dto에 적힌 값만 입력할 수 있도록
+      forbidNonWhitelisted: true,
     }),
   );
 
