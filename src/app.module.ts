09df682bc00889c70016c91bea289c09948d32c9
namju.kim <skawn11114@gmail.com>
2025-04-01 15:51:02 +0900

    Feat: static file serving app 모듈에 옵션 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,15 @@ import { CommonModule } from "./common/common.module";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
 import { ENV_DB_DATABASE_KEY, ENV_DB_HOST_KEY, ENV_DB_PASSWORD_KEY, ENV_DB_PORT_KEY, ENV_DB_USERNAME_KEY } from "./common/const/env-keys.const";
-
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { PUBLIC_FOLDER_PATH } from "./common/const/path.const";
 @Module({
   imports: [
     PostsModule,
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH, // RootPath만 지정시 localhost:3000/posts/image.jpg 으로 접근 가능 > 단점: api endpoint와 충돌 가능
+      serveRoot: "/public", //localhost:3000/public/posts/image.jpg 으로 접근 가능 > serveRoot를 지정하여서 api 주소와 차이를 줄 수 있도록 설정
+    }),
     ConfigModule.forRoot({
       envFilePath: ".env",
       isGlobal: true, //app module에서만 등록해도되도록
