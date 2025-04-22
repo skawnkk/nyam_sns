ce346f1aa22281430ee5e46b550758b4efc8ec3e
namju.kim <skawn11114@gmail.com>
2025-04-22 19:09:22 +0900

    Refactor: posts entity에서 불필요한 image 삭제
    - images로 내려받고 있기 때문

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,7 +1,4 @@
-import { Transform } from "class-transformer";
 import { IsString } from "class-validator";
-import { join } from "path";
-import { POST_PUBLIC_IMAGE_PATH } from "src/common/const/path.const";
 import { BaseModel } from "src/common/entities/base.entity";
 import { ImageModel } from "src/common/entities/image.entity";
 import { typeValidationMessage } from "src/common/validation-message/type-validation.message";
@@ -25,18 +22,12 @@ export class PostsModel extends BaseModel {
   })
   content: string;
 
-  @Column({
-    nullable: true, // 이미지가 없을 경우 null로 전달
-  })
-  @Transform(({ value }) => value && join(POST_PUBLIC_IMAGE_PATH, value))
-  image?: string;
-
   @Column()
   likeCount: number;
 
   @Column()
   commentCount: number;
 
-  @OneToMany((type) => ImageModel, (image) => image.post)
+  @OneToMany(() => ImageModel, (image) => image.post)
   images?: ImageModel[];
 }
