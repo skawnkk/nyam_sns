59d968e51bea3c514283f1cc32fde6011c804769
namju.kim <skawn11114@gmail.com>
2025-04-22 19:15:43 +0900

    Remove: CommonModule에서 MulterModule관련 처리를 하고 있으므로 삭제

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -13,11 +13,6 @@ import { AuthModule } from "src/auth/auth.module";
 import { UsersModule } from "src/users/users.module";
 import { LogMiddleware } from "src/common/middleware/log.middleware";
 import { CommonModule } from "src/common/common.module";
-import { MulterModule } from "@nestjs/platform-express";
-import { extname, join } from "path";
-import * as multer from "multer";
-import { POST_PUBLIC_IMAGE_PATH } from "src/common/const/path.const";
-import { v4 as uuid } from "uuid";
 import { ImageModel } from "src/common/entities/image.entity";
 import { PostImagesService } from "./image/images.service";
 
@@ -31,25 +26,6 @@ import { PostImagesService } from "./image/images.service";
     AuthModule,
     UsersModule,
     CommonModule,
-    MulterModule.register({
-      limits: {
-        fileSize: 1000000, // 바이트 단위
-      },
-      fileFilter: (req, file, cb) => {
-        const extension = extname(file.originalname); // 확장자 추출
-        const acceptExtensions = [".jpg", "jpeg", "png"];
-        if (!acceptExtensions.includes(extension)) {
-          return cb(new BadRequestException("jpg, jpeg, png 파일만 업로드 가능합니다."), false); //boolean -> 파일받을지 여부
-        }
-
-        return cb(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-          cb(null, join(POST_PUBLIC_IMAGE_PATH, `${uuid()}${extname(file.originalname)}`));
-        },
-      }),
-    }),
   ],
   controllers: [PostsController],
   providers: [PostsService, PostImagesService],
